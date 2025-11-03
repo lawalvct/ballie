bf0365dc04ed448db66146d48af7d4f97b85ecf9
lawalvct@gmail.com <lawalthb@gmail.com>
2025-11-03 17:46:20 +0100

    Update footer to link Privacy Policy to the terms route

--- resources/views/layouts/tenant/footer.blade.php
+++ resources/views/layouts/tenant/footer.blade.php
@@ -11,7 +11,7 @@
         </div>
 
         <div class="flex items-center space-x-4 mt-2 md:mt-0">
-            <a href="#" class="text-xs text-gray-400 hover:text-white transition-colors duration-200">Privacy Policy</a>
+            <a href="{{ route('terms') }}" class="text-xs text-gray-400 hover:text-white transition-colors duration-200">Privacy Policy</a>
             <a href="#" class="text-xs text-gray-400 hover:text-white transition-colors duration-200">Terms of Service</a>
             <a href="#" class="text-xs text-gray-400 hover:text-white transition-colors duration-200">Support</a>
         </div>
